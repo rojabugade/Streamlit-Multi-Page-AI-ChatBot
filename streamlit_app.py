dc1fe53a22e2102427ec4848914faf735d5e2e1d
Roja Bugade <69809763+rojabugade@users.noreply.github.com>
2024-09-18 22:36:33 +0000

    Keeping repo for labs only

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,17 @@
 import streamlit as st
-import HW3
 from Lab1_upload_document import upload_document
 from Lab2_ask_question import ask_question
-#from Lab3 import streaming_chatbot
+from Lab3 import streaming_chatbot
 
 # Function mapping to handle page navigation
 PAGES = {
     "Lab 1: Upload Document": upload_document,
     "Lab 2: Ask Question": ask_question,
-    #"Lab 3: Chat with ChinnuTheAIBot": streaming_chatbot,
-    "Lab 3 + HW 3: Chat with ChinnuTheAIBot": HW3.streaming_chatbot
+    "Lab 3: Chat with ChinnuTheAIBot": streaming_chatbot,
 }
 
 # Sidebar for navigation
-st.sidebar.title("HW_MAnager(Streamlit Multi-Page Chat Bot)")
+st.sidebar.title("Streamlit Multi-Page Chat Bot")
 selected_page = st.sidebar.radio("Select a Lab", list(PAGES.keys()))
 
 # Run the selected page function
